4d05c97486939225e84a6c8709a614fbacc4f34c
monalhoms-arch <monalhoms@gmail.com>
2026-04-17 23:54:08 +0200

    fix: update pdf service database connection to 127.0.0.1

--- pdf/main.py
+++ pdf/main.py
@@ -17,8 +17,8 @@ from sqlalchemy import create_engine, Column, Integer, String, DECIMAL, TIMESTAM
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root@localhost/abc"
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = "mysql+pymysql://root@127.0.0.1:3306/abc"
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
